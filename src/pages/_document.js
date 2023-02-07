7855cc5d51011f41279b1608a0a92b88670ee4ff
Mehul Kundu <programmer.mehul@gmail.com>
2023-02-07 23:28:26 +0530

    og and meta changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,24 +16,47 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* <!-- Primary Meta Tags --> */}
-          <meta name="title" content="500+ Fueler Wallpaper Collection" />
-          <meta name="description" content="Discover new and freshly looking mobile and laptop wallpapers. Time to give a makeover to your system" />
-
           {/* <!-- Facebook Meta Tags --> */}
-          <meta property="og:url" content="https://fueler-wallpaper.vercel.app/" />
+          <meta
+            property="og:url"
+            content="https://https://fueler-wallpaper.netlify.app/"
+          />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="500+ Fueler Wallpaper Collection" />
-          <meta property="og:description" content="Discover new and freshly looking mobile and laptop wallpapers. Time to give a makeover to your system" />
-          <meta property="og:image" content="https://raw.githubusercontent.com/Susmita-Dey/Fueler-Wallpaper/main/public/og-image.png?token=GHSAT0AAAAAAB5PVM5IVH2P2PMD3BOWYFLSY7CGTBQ" />
+          <meta
+            property="og:title"
+            content="500+ Fueler Wallpaper Collection"
+          />
+          <meta
+            property="og:description"
+            content="Discover new and freshly looking mobile and laptop wallpapers. Time to give a makeover to your system"
+          />
+          <meta
+            property="og:image"
+            content="https://s3.us-east-2.amazonaws.com/fueler.io-images/fueler-wallpapers/fueler-wallpaper-meta-image.jpg"
+          />
 
           {/* <!-- Twitter Meta Tags --> */}
           <meta name="twitter:card" content="summary_large_image" />
-          <meta property="twitter:domain" content="fueler-wallpaper.vercel.app" />
-          <meta property="twitter:url" content="https://fueler-wallpaper.vercel.app/" />
-          <meta name="twitter:title" content="500+ Fueler Wallpaper Collection" />
-          <meta name="twitter:description" content="Discover new and freshly looking mobile and laptop wallpapers. Time to give a makeover to your system" />
-          <meta name="twitter:image" content="https://raw.githubusercontent.com/Susmita-Dey/Fueler-Wallpaper/main/public/og-image.png?token=GHSAT0AAAAAAB5PVM5IVH2P2PMD3BOWYFLSY7CGTBQ" />
+          <meta
+            property="twitter:domain"
+            content="fueler-wallpaper.vercel.app"
+          />
+          <meta
+            property="twitter:url"
+            content="https://https://fueler-wallpaper.netlify.app/"
+          />
+          <meta
+            name="twitter:title"
+            content="500+ Fueler Wallpaper Collection"
+          />
+          <meta
+            name="twitter:description"
+            content="Discover new and freshly looking mobile and laptop wallpapers. Time to give a makeover to your system"
+          />
+          <meta
+            name="twitter:image"
+            content="https://s3.us-east-2.amazonaws.com/fueler.io-images/fueler-wallpapers/fueler-wallpaper-meta-image.jpg"
+          />
 
           <link rel="icon" href="/icon.png" />
           <style>{loader}</style>
--- src/pages/makers.js
+++ src/pages/makers.js
@@ -13,24 +13,13 @@ export default function Makers() {
       <Head>
         <title>500+ Fueler Wallpaper Collection | Makers</title>
         <meta
-          name="description"
-          content="Discover new and freshly looking mobile and laptop wallpapers. Time to give a makeover to your system"
+          name="title"
+          content="500+ Fueler Wallpaper Collection | Makers"
         />
-
-        {/* <!-- Facebook Meta Tags --> */}
-        <meta property="og:url" content="https://fueler-wallpaper.vercel.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="500+ Fueler Wallpaper Collection" />
-        <meta property="og:description" content="Discover new and freshly looking mobile and laptop wallpapers. Time to give a makeover to your system" />
-        <meta property="og:image" content="https://raw.githubusercontent.com/Susmita-Dey/Fueler-Wallpaper/main/public/og-image.png?token=GHSAT0AAAAAAB5PVM5IVH2P2PMD3BOWYFLSY7CGTBQ" />
-
-        {/* <!-- Twitter Meta Tags --> */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="fueler-wallpaper.vercel.app" />
-        <meta property="twitter:url" content="https://fueler-wallpaper.vercel.app/" />
-        <meta name="twitter:title" content="500+ Fueler Wallpaper Collection" />
-        <meta name="twitter:description" content="Discover new and freshly looking mobile and laptop wallpapers. Time to give a makeover to your system" />
-        <meta name="twitter:image" content="https://raw.githubusercontent.com/Susmita-Dey/Fueler-Wallpaper/main/public/og-image.png?token=GHSAT0AAAAAAB5PVM5IVH2P2PMD3BOWYFLSY7CGTBQ" />
+        <meta
+          name="description"
+          content="Rockstar Makers Behind Fueler Wallpaper Collection"
+        />
       </Head>
 
       <section className="mt-10">
