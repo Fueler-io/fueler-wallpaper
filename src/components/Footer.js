adfcc92da8f569e654892b2c01e15d01c55b49f5
Mehul Kundu <programmer.mehul@gmail.com>
2023-02-08 11:50:19 +0530

    typo in the footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,17 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Footer() {
-    return (
-        <footer className="bg-[#fafafa] mx-auto flex flex-col justify-center items-center mt-10 pt-10 pb-10">
-            <p className="text-center">
-                <span className="font-semibold text-[#5A5961]">Build with 💖 By Team Fueler</span>
-                <br />
-                <strong className="text-primary-blue hover:text-[#fb25ff] "><Link href="/makers">Meet the {" "} Creators</Link></strong>
-            </p>
-        </footer>
-    );
+  return (
+    <footer className="bg-[#fafafa] mx-auto flex flex-col justify-center items-center mt-10 pt-10 pb-10">
+      <p className="text-center">
+        <span className="font-semibold text-[#5A5961]">
+          Built with 💖 By Team <Link href="https://fueler.io">Fueler</Link>
+        </span>
+        <br />
+        <strong className="text-primary-blue hover:text-[#fb25ff] ">
+          <Link href="/makers">Meet the Creators</Link>
+        </strong>
+      </p>
+    </footer>
+  );
 }
