732e7b85228c7795dc744c87a911fbb356665643
Mehul Kundu <programmer.mehul@gmail.com>
2023-02-08 15:42:27 +0530

    credit added in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,21 @@ export default function Footer() {
     <footer className="bg-[#fafafa] mx-auto flex flex-col justify-center items-center mt-10 pt-10 pb-10">
       <p className="text-center">
         <span className="font-semibold text-[#5A5961]">
-          Built with 💖 By Team <Link href="https://fueler.io">Fueler</Link>
+          Built with 💖 By Team{' '}
+          <Link href="https://fueler.io" target={'_blank'}>
+            Fueler
+          </Link>
         </span>
         <br />
         <strong className="text-primary-blue hover:text-[#fb25ff] ">
           <Link href="/makers">Meet the Creators</Link>
         </strong>
+        Credits reserved to all the respective artists who have created these
+        masterpiece. Please{' '}
+        <Link href="/makers" target={'_blank'}>
+          dm
+        </Link>{' '}
+        for removal or credits.
       </p>
     </footer>
   );
